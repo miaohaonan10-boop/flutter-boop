c9d520774f2c5bfd562eeb7bad97aea24333752c
miaohaonan10-boop <miaohaonan10@gmail.com>
2026-03-30 14:23:16 +0900

    start

---
+++ lib/new5.dart
@@ -0,0 +1,9 @@
+void main() {
+  
+  int num1 = 10;
+  double num2 = 3.141592;
+
+
+  print("num1 to str is ${num1.toString()}");
+  print("num2 to str is ${num2.toStringAsFixed(2)}");
+}
---
+++ lib/new6.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  String str1 = "-12";
+  String str2 = "123.4567";
+  String str3 = "-34.56";
+
+
+  int num1 = int.parse(str1);
+  double num2 = double.parse(str2);
+  num num3 = num.parse(str3);
+
+
+  print(num1);
+  print(num2);
+  print(num3);
+}
